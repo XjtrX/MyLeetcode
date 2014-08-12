9fdb4ca92d38e980ac0869ad06e81d1b954c85f5
XjtrX <jiataorui@126.com>
2014-08-12 15:28:51 +0800

    correct the main.cpp collsion

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 #include "maxPathSum.h"
 #include "twoSum.h"
-<<<<<<< HEAD
+
 #include "treeSumCloset.h"
-=======
+
 #include "FourSum.h"
->>>>>>> master
+
 using namespace std;
 
 
@@ -22,11 +22,8 @@ struct ListNode
 
 int main()
 {
-<<<<<<< HEAD
-	treeSumCloset::Solution test;
-=======
+
 	FourSum::Solution test;
->>>>>>> master
 	test.test();
 	std::cout<<"End"<<std::endl;
 	return 0;
